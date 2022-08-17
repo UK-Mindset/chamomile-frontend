3a55070fdcad13620025e6c99222945fd0c5a7a0
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-08-17 03:07:25 +0100

    2nd Compelate choice-emotion and Edit mood.js

---
+++ mood/mood.js
@@ -0,0 +1,25 @@
+var div2 = document.getElementsByClassName("clickChange");
+
+function handleClick(event) {
+  console.log(event.target);
+  // console.log(this);
+  // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+  console.log(event.target.classList);
+
+  if (event.target.classList[1] == "clicked") {
+    event.target.classList.remove("clicked");
+  } else {
+    for (var i = 0; i < div2.length; i++) {
+      div2[i].classList.remove("clicked");
+    }
+
+    event.target.classList.add("clicked");
+  }
+}
+
+function change() {
+  for (var i = 0; i < div2.length; i++) {
+    div2[i].addEventListener("click", handleClick);
+  }
+}
